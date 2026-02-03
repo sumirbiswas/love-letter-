241b8c5868a5fea19dcf9f33fce8e05cfa1b1859
sumirbiswas <sumir.biswas2005@gmail.com>
2026-02-04 04:24:49 +0530

    Add floating petals effect

--- js/script.js
+++ js/script.js
@@ -19,3 +19,20 @@ const revealOnScroll = () => {
 
 window.addEventListener("scroll", revealOnScroll);
 revealOnScroll();
+// Floating petals
+function createPetal() {
+  const petal = document.createElement("div");
+  petal.className = "petal";
+  petal.innerHTML = "🌸";
+
+  petal.style.left = Math.random() * window.innerWidth + "px";
+  petal.style.animationDuration = 6 + Math.random() * 6 + "s";
+
+  document.body.appendChild(petal);
+
+  setTimeout(() => {
+    petal.remove();
+  }, 12000);
+}
+
+setInterval(createPetal, 800);
